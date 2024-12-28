8f612862b7afa9590d00c97e42fd66514a61c82b
awais512 <raza.awais25@gmail.com>
2024-12-28 11:52:08 +0500

    Task List and List in tiptap

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -3,6 +3,8 @@
 import React from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
 
 export const Editor = () => {
   const editor = useEditor({
@@ -13,7 +15,7 @@ export const Editor = () => {
           "focus:outline-none print:border-0 bg-white border border-[#c7c7c7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
-    extensions: [StarterKit],
+    extensions: [StarterKit, TaskItem.configure({ nested: true }), TaskList],
     content: "<p>Hello World! 🌎️</p>",
   });
 
