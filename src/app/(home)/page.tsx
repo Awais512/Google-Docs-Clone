80d2aa142db337601bd9e342e6ad7d44d96c2bc9
awais512 <raza.awais25@gmail.com>
2025-01-15 13:31:28 +0500

    Add convex and authentication with convex react

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Navbar } from "./navbar";
 import { TemplatesGallery } from "./templates-gallery";
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
 
 export default function Home() {
+  const documents = useQuery(api.documents.get);
+
+  if (!documents) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -12,6 +22,9 @@ export default function Home() {
       <div className="mt-16">
         <TemplatesGallery />
       </div>
+      {documents.map((doc) => (
+        <p key={doc._id}>{doc.title}</p>
+      ))}
     </div>
   );
 }
